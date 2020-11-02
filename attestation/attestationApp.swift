77f911c060f1641b005e5811250cefd94d852eb9
Cyrille Legrand <c.legrand@useradgents.com>
2020-11-02 08:51:04 +0100

    Initial Commit

---
+++ attestation/attestationApp.swift
@@ -0,0 +1,17 @@
+//
+//  attestationApp.swift
+//  attestation
+//
+//  Created by Cyrille Legrand on 02/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct attestationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
